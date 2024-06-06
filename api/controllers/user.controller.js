762a512f3a17516bea2dcc104ff8211a4a172fbc
Chandra Sekhar <cstangirala2003@gmail.com>
2024-06-06 08:54:52 +0530

    created a dummy test api to check the functionality of express.

---
+++ api/controllers/user.controller.js
@@ -0,0 +1,4 @@
+const test = (req , res)=>{
+    res.json({message:"API is working !!"})
+}
+export default test
--- api/index.js
+++ api/index.js
@@ -1,8 +1,10 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
+import userRoutes from './routes/user.route.js'
 
 dotenv.config();
+//--------------------------------------------------------
 
 mongoose.connect(process.env.MONGODBURL)
 .then(
@@ -12,7 +14,11 @@ mongoose.connect(process.env.MONGODBURL)
     (err)=>{ console.log(err)}
 )
 
+//----------------------------------------------------------
+
 const app = express();
+
+app.use('/api/user' , userRoutes);
 
 app.listen(3000 , ()=>{
     console.log("Server is running on port 3000!")
