9dc8c382fec9c15ef8ef0a8456ee27bb064c50de
Chandra Sekhar <cstangirala2003@gmail.com>
2024-06-07 11:27:40 +0530

    added functionality to sign up page

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -7,6 +7,7 @@ const signup = async(req , res , next)=>{
    const{username , email , password} = req.body;
    if(!username || !email || !password || username === '' || password === ''|| email === '')
     next(errorHandler(400 , 'All fields are required'))
+   
 
 const hashedPassword = bcryptjs.hashSync(password , 10);
 
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,8 +1,62 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { Button, Label, TextInput } from 'flowbite-react'
+import React, { useState } from 'react'
+import { Link , useNavigate } from 'react-router-dom'
+import { Button, Label, TextInput, Alert, Spinner } from 'flowbite-react'
+
 
 export default function Signup() {
+  const[formData , setFormData] = useState({})
+  const[errorMessage  , setErrorMessage] = useState(null);
+  const[loading , setLoading] = useState(false);
+  const navigate = useNavigate();
+  const handleChange = (e)=>{
+    // console.log(e.target.value);
+    setFormData({...formData, [e.target.id]: e.target.value.trim()})
+  }
+  // const handleSubmit = async(e) =>{
+  //   e.preventDefault();
+  //   try{
+  //     const res = await fetch('/api/auth/signup',{
+  //       method:'POST',
+  //       headers:{'Content-Type':'application/json'},
+  //       body: JSON.stringify(formData)
+  //     })
+  //     const data = await res.json(); 
+  //   } catch(error){
+
+  //   }
+  // }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if(!formData.username || !formData.email || !formData.password )
+      return setErrorMessage('Please fill out all the fields')
+    // console.log('Form data:', formData); // Add logging to check form data
+    try {
+      setLoading(true);
+      setErrorMessage(null);
+      const res = await fetch('/api/auth/signup', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(formData)
+      });
+      const data = await res.json();
+      if(data.success === false){
+        return setErrorMessage('username or email already exists.');
+      }
+      setLoading(false);
+      if(res.ok){
+        navigate('/sign-in')
+        
+      }
+      // console.log('Response data:', data); // Add logging to check response data
+    } catch (error) {
+      setErrorMessage(error.message);
+      setLoading(false);
+    }
+  };
+
+
+
   return (
     <div className='min-h-screen mt-20'>
 
@@ -22,21 +76,28 @@ export default function Signup() {
 
           {/* right */}
         <div className="flex-1">
-          <form className='flex flex-col gap-4'>
+          <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
             <div className="">
               <Label value='Your Username' />
-              <TextInput type='text' placeholder='Username' id='username'/>
+              <TextInput type='text' placeholder='Username' id='username'  onChange={handleChange}/>
             </div>
             <div className="">
               <Label value='Your Email' />
-              <TextInput type='email' placeholder='Email' id='email'/>
+              <TextInput type='email' placeholder='Email' id='email' onChange={handleChange}/>
             </div>
             <div className="">
               <Label value='Your Password' />
-              <TextInput type='password' placeholder='Password' id='password'/>
+              <TextInput type='password' placeholder='Password' id='password' onChange={handleChange}/>
             </div>
-            <Button gradientDuoTone='purpleToPink' type='submit'>
-              Sign Up
+            <Button gradientDuoTone='purpleToPink' type='submit' disabled={loading}>
+             {
+              loading ? (
+                <>
+                <Spinner size='sm'/>
+                <span className='pl-3'>Loading...</span>
+                </>
+              ) : 'Sign Up'
+             }
             </Button>
           </form>
           <div className="flex gap-2 text-sm mt-5">
@@ -45,6 +106,13 @@ export default function Signup() {
             Sign In
             </Link>
           </div>
+          {
+            errorMessage && (
+              <Alert className='mt-5' color='failure'>
+                {errorMessage}
+              </Alert>
+            )
+          }
 
 
 
