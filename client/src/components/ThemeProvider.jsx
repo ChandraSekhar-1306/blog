e6ae6b2e351b12505312910a29f3907779879ea1
Chandra Sekhar <cstangirala2003@gmail.com>
2024-06-08 16:34:37 +0530

    added dark mode functionality

---
+++ client/src/components/ThemeProvider.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+
+export default function ThemeProvider({children}) {
+    const {theme} = useSelector(state => state.theme)
+  return (
+    <div className={theme}>
+        <div className="bg-white text-gray-700 dark:text-gray-200 dark:bg-gray-800 min-h-screen">
+
+        {children}
+        </div>
+    </div>
+  )
+}
