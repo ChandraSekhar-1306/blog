a76ea5ecefda2d2b6c6daf5de7456a4f78962358
Chandra Sekhar <cstangirala2003@gmail.com>
2024-06-08 19:44:00 +0530

    complete profile page UI

--- client/src/components/ThemeProvider.jsx
+++ client/src/components/ThemeProvider.jsx
@@ -5,7 +5,7 @@ export default function ThemeProvider({children}) {
     const {theme} = useSelector(state => state.theme)
   return (
     <div className={theme}>
-        <div className="bg-white text-gray-700 dark:text-gray-200 dark:bg-gray-700 min-h-screen">
+        <div className="bg-white text-gray-700 dark:text-gray-200 dark:bg-gray-900 min-h-screen">
 
         {children}
         </div>
