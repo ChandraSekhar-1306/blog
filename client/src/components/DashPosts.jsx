a9548de10b24e6059718e927e7f88635cc8a803d
CS <cstangirala2003@gmail.com>
2024-12-27 15:22:40 +0530

    add delete post functionality in the dashboard

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -1,13 +1,15 @@
 import React, { useEffect, useState } from 'react'
 import{useSelector} from 'react-redux'
-import{Table} from 'flowbite-react'
+import{Table , Modal , Button} from 'flowbite-react'
 import{Link} from 'react-router-dom'
+import {HiOutlineExclamationCircle} from 'react-icons/hi'
 
 export default function DashPosts() {
     const{currentUser} = useSelector((state)=>state.user)
     const[userPosts, setUserPosts] = useState([])
     const[showMore , setShowMore] = useState(true);
-    console.log(userPosts)
+    const[showModal , setShowModal] = useState(false);
+    const[postIdToDelete,setPostIdToDelete] = useState('');
     useEffect(()=>{
         const fetchPosts = async()=>{
             try {
@@ -43,6 +45,24 @@ export default function DashPosts() {
             console.log(error)
         }
     }
+
+    const handleDeletePost = async()=>{
+            setShowModal(false);
+            try {
+                const res = await fetch(`/api/post/deletepost/${postIdToDelete}/${currentUser._id}` , {
+                    method:'DELETE',
+
+                })
+                const data = await res.json();
+                if(!res.ok){
+                    console.log(data.message);
+                } else{
+                    setUserPosts((prev)=> prev.filter(post => post._id !== postIdToDelete))
+                }
+            } catch (error) {
+                console.log(error)
+            }
+    }
   return (
     <div className='table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500'>
         {currentUser.isAdmin && userPosts.length > 0 ? (
@@ -63,7 +83,10 @@ export default function DashPosts() {
                             <Table.Cell><Link to={`/post/${post.slug}`}> <img src={post.image} alt={post.title} className='w-20 h-10 object-cover bg-gray-500' /></Link></Table.Cell>
                             <Table.Cell><Link className='font-medium text-gray-900 dark:text-white' to={`/post/${post.slug}`}>{post.title}</Link></Table.Cell>
                             <Table.Cell>{post.category}</Table.Cell>
-                            <Table.Cell><span className='font-medium text-red-500 hover:underline cursor-pointer'>Delete</span></Table.Cell>
+                            <Table.Cell><span onClick={()=>{
+                                    setShowModal(true);
+                                    setPostIdToDelete(post._id)
+                            }} className='font-medium text-red-500 hover:underline cursor-pointer'>Delete</span></Table.Cell>
                             <Table.Cell><Link className='text-teal-500 font-medium hover:underline' to={`/update-post/${post._id}`}><span>Edit</span></Link></Table.Cell>
                         </Table.Row>
                     </Table.Body>
@@ -78,6 +101,22 @@ export default function DashPosts() {
         ):(
             <p>You have no posts yet..</p>
         )}
+        <Modal show={showModal} onClose={()=> setShowModal(false)} popup size='md'>
+                <Modal.Header/>
+                <Modal.Body>
+                  <div className="text-center">
+                    <HiOutlineExclamationCircle className='h-14 w-14 text-gray-400 dark:text-gray-200 mb-4 mx-auto' />
+                    <h3 className='mb-5 text-lg text-gray-500 dark:text-gray-200'>Are you sure you want to delete your account?</h3>
+                    <div className="flex justify-center gap-4">
+                      <Button color='failure' onClick={handleDeletePost}>Yes, I'm sure</Button>
+                      <Button color='gray' onClick={()=> setShowModal(false)}>Nah, Nope never mind</Button>
+                    </div>
+                  </div>
+                </Modal.Body>
+        
+                
+                
+              </Modal>
     </div>
   )
 }
